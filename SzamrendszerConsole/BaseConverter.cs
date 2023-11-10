b0f288ff3ec457138a812cab152f03d075354d73
Bazs <92711265+BartaBlzs@users.noreply.github.com>
2023-11-10 13:44:34 +0100

    1110

---
+++ SzamrendszerConsole/BaseConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamrendszerAtvalto
+{
+	class BaseConverter
+	{
+		public string AnyConvert(string fromNumber, int fromBase, int toBase)
+		{
+			return DecimalToAnyBase(AnyBaseToDecimal(fromNumber, fromBase), toBase);
+		}
+
+		private int AnyBaseToDecimal(string anyBase, int fromBase)
+		{
+			int val(char c)
+			{
+				if (c >= '0' && c <= '9')
+					return (int)c - '0';
+				else
+					return (int)c - 'A' + 10;
+			}
+
+			int len = anyBase.Length;
+			int power = 1;
+			int num = 0;
+			int i;
+
+			for (i = len - 1; i >= 0; i--)
+			{
+
+				if (val(anyBase[i]) >= fromBase)
+				{
+					return -1;
+				}
+
+				num += val(anyBase[i]) * power;
+				power = power * fromBase;
+			}
+			return num;
+		}
+
+		private string DecimalToAnyBase(int decimalNumber, int toBase)
+		{
+			string result = string.Empty;
+			do
+			{
+				result = "0123456789ABCDEF"[decimalNumber % toBase] + result;
+				decimalNumber /= toBase;
+			}
+			while (decimalNumber > 0);
+
+			return result;
+		}
+	}
+}
---
+++ SzamrendszerConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamrendszerAtvalto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseConverter converter = new BaseConverter();
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Az átváltandó szám: ");
+                    var fromNumber = Console.ReadLine();
+
+                    Console.Write("Az alap számrendszer: ");
+                    var fromBase = int.Parse(Console.ReadLine());
+
+                    Console.Write("A vég számrendszer: ");
+                    var toBase = int.Parse(Console.ReadLine());
+                    var res = converter.AnyConvert(fromNumber, fromBase, toBase);
+                    Console.WriteLine($"{res}\n");
+                    File.AppendAllText("konzol_valtasok.txt", $"{DateTime.Now.ToLocalTime()}\n{fromNumber} ({fromBase}) => {res} ({toBase})\n\n");
+                }
+                catch
+                {
+                    Console.WriteLine("Hibás számot adtál meg!\n");
+                }
+                
+            }
+        }
+    }
+}
